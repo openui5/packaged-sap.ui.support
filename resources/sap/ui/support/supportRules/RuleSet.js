f182b9026d6af166a125f3d9bd42e6dba5f466cc
OpenUI5 <openui5@sap.com>
2018-03-15 18:51:36 +0100

    1.52.9

--- resources/sap/ui/support/supportRules/RuleSet.js
+++ resources/sap/ui/support/supportRules/RuleSet.js
@@ -37,7 +37,7 @@ function (jQuery, storage, constants) {
 	 * @name sap.ui.support.RuleSet
 	 * @memberof sap.ui.support
 	 * @author SAP SE
-	 * @version 1.52.8
+	 * @version 1.52.9
 	 * @param {object} oSettings Name of the initiated
 	 * @returns {void}
 	 */
