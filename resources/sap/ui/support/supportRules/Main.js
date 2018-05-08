5f8528d0a28137a5eb1a96cf04a265ec361442ae
OpenUI5 <openui5@sap.com>
2018-05-08 18:31:47 +0200

    1.54.5

--- resources/sap/ui/support/supportRules/Main.js
+++ resources/sap/ui/support/supportRules/Main.js
@@ -60,7 +60,7 @@ function (jQuery, ManagedObject, JSONModel, Analyzer, CoreFacade,
 				 * @namespace
 				 * @alias jQuery.sap.support
 				 * @author SAP SE
-				 * @version 1.54.4
+				 * @version 1.54.5
 				 * @public
 				 */
 				jQuery.sap.support = {
--- resources/sap/ui/support/supportRules/Storage.js
+++ resources/sap/ui/support/supportRules/Storage.js
@@ -22,7 +22,7 @@ sap.ui.define([
  * @name sap.ui.support.Storage
  * @alias sap.ui.support.Storage
  * @author SAP SE.
- * @version 1.54.4
+ * @version 1.54.5
  *
  * @private
  *
