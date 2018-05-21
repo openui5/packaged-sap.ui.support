b73038296d5e14568de67cb8c777aa4fd6911326
OpenUI5 <openui5@sap.com>
2018-05-21 17:11:16 +0200

    1.52.13

--- resources/sap/ui/support/supportRules/WindowCommunicationBus.js
+++ resources/sap/ui/support/supportRules/WindowCommunicationBus.js
@@ -21,7 +21,7 @@ function (jQuery) {
 	 * @name sap.ui.support.WindowCommunicationBus
 	 * @memberof sap.ui.support
 	 * @author SAP SE
-	 * @version 1.52.12
+	 * @version 1.52.13
 	 * @private
 	 */
 	var CommunicationBus = {
