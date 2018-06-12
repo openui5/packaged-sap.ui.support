5638df0a102895e89d5a7e076b4f93f6b69a2d28
OpenUI5 <openui5@sap.com>
2018-06-12 11:45:04 +0200

    1.56.2

--- resources/sap/ui/support/supportRules/Main.js
+++ resources/sap/ui/support/supportRules/Main.js
@@ -60,7 +60,7 @@ function (jQuery, ManagedObject, JSONModel, Analyzer, CoreFacade,
 				 * @namespace
 				 * @alias jQuery.sap.support
 				 * @author SAP SE
-				 * @version 1.56.1
+				 * @version 1.56.2
 				 * @public
 				 */
 				jQuery.sap.support = {
