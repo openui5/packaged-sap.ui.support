4233eadf7b0743d0abfb59bd79de55eae6c77734
OpenUI5 <openui5@sap.com>
2018-07-03 11:29:13 +0200

    1.56.3

--- resources/sap/ui/support/supportRules/Main.js
+++ resources/sap/ui/support/supportRules/Main.js
@@ -60,7 +60,7 @@ function (jQuery, ManagedObject, JSONModel, Analyzer, CoreFacade,
 				 * @namespace
 				 * @alias jQuery.sap.support
 				 * @author SAP SE
-				 * @version 1.56.2
+				 * @version 1.56.3
 				 * @public
 				 */
 				jQuery.sap.support = {
--- resources/sap/ui/support/supportRules/WindowCommunicationBus.js
+++ resources/sap/ui/support/supportRules/WindowCommunicationBus.js
@@ -21,7 +21,7 @@ function (jQuery) {
 	 * @name sap.ui.support.WindowCommunicationBus
 	 * @memberof sap.ui.support
 	 * @author SAP SE
-	 * @version 1.56.2
+	 * @version 1.56.3
 	 * @private
 	 */
 	var CommunicationBus = {
