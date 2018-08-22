70ba4485ff53e92f96d19ed40841dc2ed8b10da1
OpenUI5 <openui5@sap.com>
2018-08-22 13:36:43 +0200

    1.56.7

--- resources/sap/ui/support/supportRules/Main.js
+++ resources/sap/ui/support/supportRules/Main.js
@@ -60,7 +60,7 @@ function (jQuery, ManagedObject, JSONModel, Analyzer, CoreFacade,
 				 * @namespace
 				 * @alias jQuery.sap.support
 				 * @author SAP SE
-				 * @version 1.56.6
+				 * @version 1.56.7
 				 * @public
 				 */
 				jQuery.sap.support = {
--- resources/sap/ui/support/supportRules/RuleSet.js
+++ resources/sap/ui/support/supportRules/RuleSet.js
@@ -37,7 +37,7 @@ function (jQuery, storage, constants) {
 	 * @name sap.ui.support.RuleSet
 	 * @memberof sap.ui.support
 	 * @author SAP SE
-	 * @version 1.56.6
+	 * @version 1.56.7
 	 * @param {object} oSettings Name of the initiated
 	 * @returns {void}
 	 */
