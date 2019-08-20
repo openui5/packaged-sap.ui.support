85f5336792f7b0568135b2e2e758eb85627d78e0
OpenUI5 <openui5@sap.com>
2019-08-20 17:59:02 +0200

    1.52.32

--- resources/sap/ui/support/supportRules/RuleSet.js
+++ resources/sap/ui/support/supportRules/RuleSet.js
@@ -37,7 +37,7 @@ function (jQuery, storage, constants) {
 	 * @name sap.ui.support.RuleSet
 	 * @memberof sap.ui.support
 	 * @author SAP SE
-	 * @version 1.52.31
+	 * @version 1.52.32
 	 * @param {object} oSettings Name of the initiated
 	 * @returns {void}
 	 */
--- resources/sap/ui/support/supportRules/WindowCommunicationBus.js
+++ resources/sap/ui/support/supportRules/WindowCommunicationBus.js
@@ -21,7 +21,7 @@ function (jQuery) {
 	 * @name sap.ui.support.WindowCommunicationBus
 	 * @memberof sap.ui.support
 	 * @author SAP SE
-	 * @version 1.52.31
+	 * @version 1.52.32
 	 * @private
 	 */
 	var CommunicationBus = {
